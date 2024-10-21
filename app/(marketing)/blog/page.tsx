9c930e14becfa48740051db823da58d9fab2a236
morioka ryosuke <morioka@details.co.jp>
2024-10-21 23:49:59 +0900

    update blog一覧ページ

---
+++ app/(marketing)/blog/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { allPosts } from '@/.contentlayer/generated/';
+import { format } from 'date-fns';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const BlogPage = () => {
+  const posts = allPosts;
+  return (
+    <div className="container max-w-4xl py-4 lg:py-10">
+      <div>
+        <div className="space-y-4">
+          <h1 className="font-extrabold text-4xl lg:text-5xl tracking-tight">Blog</h1>
+          <p className="text-muted-foreground text-xl">ブログを追加します</p>
+        </div>
+        <hr className="my-8" />
+        <div className="grid sm:grid-cols-2 gap-10">
+          {posts.map((post) => (
+            <article key={post._id} className="relative flex flex-col space-y-2">
+              {post.image && <Image src={post.image} alt={post.title} width={804} height={452} className="rounded-md border bg-muted" />}
+              <h2 className="text-2xl font-extrabold">{post.title}</h2>
+              {post.description && <p className="text-muted-foreground">{post.description}</p>}
+              {post.date && <p className="text-sm text-muted-foreground">{format(post.date, 'yyyy/MM/dd')}</p>}
+              <Link href={post.slug} className="absolute inset-0"></Link>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BlogPage
